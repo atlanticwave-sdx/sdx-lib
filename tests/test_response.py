8b9ed6f3cdaf9f087e0f3cb9a64eddf0ec372ea8
SColson82 <sharon.colson@gmail.com>
2024-10-15 06:11:20 -0500

    reformatted

--- tests/test_response.py
+++ tests/test_response.py
@@ -2,8 +2,8 @@ import unittest
 from sdxlib.sdx_response import SDXResponse
 from test_config import *
 
-class SDXResponseTest(unittest.TestCase):
 
+class SDXResponseTest(unittest.TestCase):
     def test_response_initialization_with_valid_json(self):
         response_json = {
             "service_id": TEST_SERVICE_ID,
@@ -14,14 +14,16 @@ class SDXResponseTest(unittest.TestCase):
             "state": "enabled",
             "counters_location": "https://my.aw-sdx.net/l2vpn/7cdf23e8978c",
             "last_modified": "0",
-            "current_path": ["urn:sdx:link:tenet.ac.za:LinkToSAX",    
-                 "urn:sdx:link:tenet.ac.za:LinkToAmpath",  
-                 "urn:sdx:link:ampath.net:LinkToSAX"],
+            "current_path": [
+                "urn:sdx:link:tenet.ac.za:LinkToSAX",
+                "urn:sdx:link:tenet.ac.za:LinkToAmpath",
+                "urn:sdx:link:ampath.net:LinkToSAX",
+            ],
             "oxp_service_ids": {
-                    "AmLight.net": ["c73da8e1"], 
-                    "TENET.ac.za": ["5d034620"], 
-                    "SAX.br": ["7cdf23e8978c"]
-            }
+                "AmLight.net": ["c73da8e1"],
+                "TENET.ac.za": ["5d034620"],
+                "SAX.br": ["7cdf23e8978c"],
+            },
         }
         response = SDXResponse(response_json)
         self.assertEqual(response.service_id, TEST_SERVICE_ID)
@@ -30,23 +32,29 @@ class SDXResponseTest(unittest.TestCase):
         self.assertEqual(response.archived_date, "0")
         self.assertEqual(response.status, "up")
         self.assertEqual(response.state, "enabled")
-        self.assertEqual(response.counters_location, "https://my.aw-sdx.net/l2vpn/7cdf23e8978c")
+        self.assertEqual(
+            response.counters_location, "https://my.aw-sdx.net/l2vpn/7cdf23e8978c"
+        )
         self.assertEqual(response.last_modified, "0")
-        self.assertEqual(response.current_path, ["urn:sdx:link:tenet.ac.za:LinkToSAX",    
-                 "urn:sdx:link:tenet.ac.za:LinkToAmpath",  
-                 "urn:sdx:link:ampath.net:LinkToSAX"]
-                 )
-        self.assertEqual(response.oxp_service_ids, {
-                    "AmLight.net": ["c73da8e1"], 
-                    "TENET.ac.za": ["5d034620"], 
-                    "SAX.br": ["7cdf23e8978c"]
-            }
+        self.assertEqual(
+            response.current_path,
+            [
+                "urn:sdx:link:tenet.ac.za:LinkToSAX",
+                "urn:sdx:link:tenet.ac.za:LinkToAmpath",
+                "urn:sdx:link:ampath.net:LinkToSAX",
+            ],
+        )
+        self.assertEqual(
+            response.oxp_service_ids,
+            {
+                "AmLight.net": ["c73da8e1"],
+                "TENET.ac.za": ["5d034620"],
+                "SAX.br": ["7cdf23e8978c"],
+            },
         )
 
     def test_response_initialization_with_missing_attributes(self):
-        response_json = {
-            "service_id": "12345678-abcd-efgh-ijkl-mnopqrstuvwxyz"
-        }
+        response_json = {"service_id": "12345678-abcd-efgh-ijkl-mnopqrstuvwxyz"}
         response = SDXResponse(response_json)
         self.assertIsNone(response.archived_date)
         self.assertIsNone(response.counters_location)
@@ -55,4 +63,3 @@ class SDXResponseTest(unittest.TestCase):
         response_json = "invalid_json"
         with self.assertRaises(TypeError):
             SDXResponse(response_json)
-
