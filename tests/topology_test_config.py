1bfa15f910c324833297118dd3d89ec9c373ad72
SColson82 <sharon.colson@gmail.com>
2025-02-23 17:11:10 -0600

    adds more config variables

--- tests/topology_test_config.py
+++ tests/topology_test_config.py
@@ -5,14 +5,23 @@ sample_location = Location(
     address="123 Main St", latitude=37.7749, longitude=-122.4194, iso3166_2_lvl4="US-FL"
 )
 
+sample_services = {
+    "l2vpn-ptp": {"vlan_range": [[1, 4095]]},
+    "l2vpn-ptmp": {},
+}
+
 # **Valid Ports**
 sample_port1 = Port(
     name="Port1",
     id="urn:sdx:port:amlight.net:Node1:Port1",
     node="urn:sdx:node:amlight.net:Node1",
     type=PortType.GE_10,
+    mtu=10000,
+    nni="",
     status=Status.UP,
     state=State.ENABLED,
+    services={"l2vpn-ptp": {"vlan_range": [[1, 4095]]}, "l2vpn-ptmp": {},},
+    entities=["Sprace", "UNESP", "LHC Tier 2 Brazil"],
 )
 
 sample_port2 = Port(
@@ -20,10 +29,33 @@ sample_port2 = Port(
     id="urn:sdx:port:amlight.net:Node2:Port2",
     node="urn:sdx:node:amlight.net:Node2",
     type=PortType.GE_10,
+    mtu=1500,
+    nni="",
     status=Status.UP,
     state=State.ENABLED,
+    services=sample_services,
     entities=["Sprace", "UNESP", "LHC Tier 2 Brazil"],
 )
+
+# "ports": [
+#                 {
+#                     "id": "urn:sdx:port:ampath.net:Ampath3:50",
+#                     "name": "Ampath3-eth50",
+#                     "node": "urn:sdx:node:ampath.net:Ampath3",
+#                     "status": Status.UP,
+#                     "state": State.ENABLED,
+#                     "mtu": 1500,  # Added `mtu`
+#                     "nni": None,
+#                     "type": PortType.GE_10,
+#                     "services": {
+#                         "l2vpn-ptp": {"vlan_range": [[1, 4095]]},
+#                         "l2vpn-ptmp": {}
+#                     },
+#                     "entities": [
+#                         "urn:sdx:entity:ampath"
+#                     ],  # `entities` moved into `Port`
+#                 }
+#             ],
 
 # **Invalid Ports for Testing**
 invalid_port_long_name = {
@@ -120,6 +152,69 @@ valid_topology = {
     "nodes": valid_nodes,
     "links": valid_links,
 }
+
+MOCK_TOPOLOGY = {
+    "id": "urn:sdx:topology:ampath.oxp",  # Follows `URN_TOPOLOGY_PATTERN`
+    "name": "Ampath-OXP",
+    "version": 1,  # Starts at 1, controller increments it
+    "model_version": MODEL_VERSION,  # Added correct model version
+    "timestamp": "2024-11-09T20:40:47Z",  # Corrected timestamp format
+    # Ensured `services` only contains allowed values
+    "services": ["l2vpn-ptp", "l2vpn-ptmp"],
+    "nodes": [
+        {
+            "id": "urn:sdx:node:ampath.net:Ampath3",
+            "name": "Ampath3",
+            "status": Status.UP,  # Added `status`
+            "state": State.ENABLED,  # Added `state`
+            "location": {
+                "address": "Jacksonville",
+                "latitude": 30.27,
+                "longitude": -81.68,
+                "iso3166_2_lvl4": "US-FL",
+            },
+            "ports": [
+                {
+                    "id": "urn:sdx:port:ampath.net:Ampath3:50",
+                    "name": "Ampath3-eth50",
+                    "node": "urn:sdx:node:ampath.net:Ampath3",
+                    "status": Status.UP,
+                    "state": State.ENABLED,
+                    "mtu": 1500,  # Added `mtu`
+                    "nni": None,
+                    "type": PortType.GE_10,
+                    "services": {
+                        "l2vpn-ptp": {"vlan_range": [[1, 4095]]},
+                        "l2vpn-ptmp": {},
+                    },
+                    "entities": [
+                        "Florida International University"
+                    ],  # `entities` moved into `Port`
+                }
+            ],
+        }
+    ],
+    "links": [
+        {
+            "id": "urn:sdx:link:ampath.net:Ampath1/2_Ampath3/2",
+            "name": "Ampath1/2_Ampath3/2",
+            "type": LinkType.INTRA,  # Added `type`
+            "ports": [
+                "urn:sdx:port:ampath.net:Ampath1:2",
+                "urn:sdx:port:ampath.net:Ampath3:2",
+            ],
+            "bandwidth": 10,
+            "residual_bandwidth": 100,
+            "latency": 0,
+            "packet_loss": 0,
+            "availability": 0,
+            "status": Status.UP,
+            "state": State.ENABLED,
+            "private": [],  # Added `private` (default empty list)
+        }
+    ],
+}
+
 
 # **Invalid Topology (Empty Nodes)**
 invalid_topology_empty_nodes = {
