2adc806bbb8f14dfe830e4e0a52f5f1c8656e71e
SColson82 <sharon.colson@gmail.com>
2025-03-29 18:40:27 -0500

    adds working test to validate that base url is supplied when creating client

--- tests/test_create_l2vpn.py
+++ tests/test_create_l2vpn.py
@@ -10,35 +10,11 @@ Test the SDXClient class for creating L2VPNs.
 - behaves correctly when the API returns a success response
 """
 
-
-@pytest.fixture
-def base_url():
-    return "http://aw-sdx-controller.renci.org:8081/SDX-Controller"
-
-
-@pytest.fixture
-def service_name():
-    return "TestL2VPN"
-
-
-@pytest.fixture
-def service_endpoints():
-    return [
-        {"port_id": "urn:sdx:port:tenet.ac.za:Tenet03:50", "vlan": "150"},
-        {"port_id": "urn:sdx:port:ampath.net:Ampath3:50", "vlan": "300"},
-    ]
-
-
-@pytest.fixture
-def expected_service_id():
-    return "e64f7146-5673-4e03-9d6d-95eafab657f9"
-
-
 @patch("sdxlib.sdx_token_auth.TokenAuthentication.load_token")
 def test_create_l2vpn_successful_request(
     mock_load_token, base_url, service_name, service_endpoints, expected_service_id
 ):
-
+    
     mock_load_token.return_value.token_eppn = "pi@sdx-email.org"
     client = SDXClient(
         base_url=base_url,
@@ -55,3 +31,9 @@ def test_create_l2vpn_successful_request(
 
         result = client.create_l2vpn()
         assert result["service_id"] == expected_service_id
+
+def test_create_l2vpn_fails_with_missing_base_url(service_name, service_endpoints):
+    with pytest.raises(ValueError, match="Base URL must be a non-empty string."):
+        SDXClient(base_url=None, name=service_name, endpoints=service_endpoints)
+
+
