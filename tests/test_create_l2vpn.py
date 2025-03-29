3b706ce3d2654535a33fecc0df7c6163fa3e41fb
SColson82 <sharon.colson@gmail.com>
2025-03-29 17:49:23 -0500

    add pytest fixtures

--- tests/test_create_l2vpn.py
+++ tests/test_create_l2vpn.py
@@ -11,26 +11,47 @@ Test the SDXClient class for creating L2VPNs.
 """
 
 
+@pytest.fixture
+def base_url():
+    return "http://aw-sdx-controller.renci.org:8081/SDX-Controller"
+
+
+@pytest.fixture
+def service_name():
+    return "TestL2VPN"
+
+
+@pytest.fixture
+def service_endpoints():
+    return [
+        {"port_id": "urn:sdx:port:tenet.ac.za:Tenet03:50", "vlan": "150"},
+        {"port_id": "urn:sdx:port:ampath.net:Ampath3:50", "vlan": "300"},
+    ]
+
+
+@pytest.fixture
+def expected_service_id():
+    return "e64f7146-5673-4e03-9d6d-95eafab657f9"
+
+
 @patch("sdxlib.sdx_token_auth.TokenAuthentication.load_token")
-def test_create_l2vpn_successful_request(mock_load_token):
+def test_create_l2vpn_successful_request(
+    mock_load_token, base_url, service_name, service_endpoints, expected_service_id
+):
+
     mock_load_token.return_value.token_eppn = "pi@sdx-email.org"
-    # Setup fake SDXClient
     client = SDXClient(
-        base_url="http://aw-sdx-controller.renci.org:8081/SDX-Controller",
-        name="TestL2VPN",
-        endpoints=[
-            {"port_id": "urn:sdx:port:tenet.ac.za:Tenet03:50", "vlan": "150"},
-            {"port_id": "urn:sdx:port:ampath.net:Ampath3:50", "vlan": "300"},
-        ],
+        base_url=base_url,
+        name=service_name,
+        endpoints=service_endpoints,
     )
 
-    # Mock the API endpoint
     with requests_mock.Mocker() as m:
         m.post(
-            "http://aw-sdx-controller.renci.org:8081/SDX-Controller/l2vpn/1.0",
-            json={"service_id": "e64f7146-5673-4e03-9d6d-95eafab657f9"},
+            f"{base_url}/l2vpn/1.0",
+            json={"service_id": expected_service_id},
             status_code=201,
         )
 
         result = client.create_l2vpn()
-        assert result["service_id"] == "e64f7146-5673-4e03-9d6d-95eafab657f9"
+        assert result["service_id"] == expected_service_id
