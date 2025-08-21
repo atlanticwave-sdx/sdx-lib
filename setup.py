e7918586a495d17befa6c230058969d2832b287a
Italo Valcy <italovalcy@gmail.com>
2025-08-21 17:27:07 -0300

    fixed version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="sdxlib",
-    version="0.1",
+    version="0.35.1",
     packages=find_packages(),
     install_requires=["requests", "pycountry", "dacite",],
 )
